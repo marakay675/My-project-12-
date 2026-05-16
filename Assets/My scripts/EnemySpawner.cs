68dd3504311268791b50233ad203d03193fc897b
marakay675 <marakay675@gmail.com>
2026-05-16 02:50:59 -0400

    s

--- Assets/My scripts/EnemySpawner.cs
+++ Assets/My scripts/EnemySpawner.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _prefab;
-    //[SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private bool _canSpawn = true;
     [SerializeField] private Transform _targetForMyEnemies;
 
@@ -16,14 +15,8 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator CoroutineSpawn()
     {
-        //int minSpawnIndex = 0;
-
         while (_canSpawn == true)
         {
-            //int randomIndex = Random.Range(minSpawnIndex, _spawnPoints.Length);
-
-            //Vector3 spawnPosition = _spawnPoints[randomIndex].position;
-
            GameObject newEnemy = Instantiate(_prefab, transform.position, Quaternion.LookRotation(Vector3.right));
             EnemyController controller = newEnemy.GetComponent<EnemyController>();
 
